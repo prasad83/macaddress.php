64f07ef92a9432434d66dfde4c84fa89716ea22e
prasad83 <prasad83@users.noreply.github.com>
2014-06-12 17:19:25 +0530

    Enhanced detection specific to WIN / *NIX.

--- macaddress.php
+++ macaddress.php
@@ -7,10 +7,18 @@
  * @return macaddress of the first successfully detected interface.
  */
 function findmacaddress($ifaces=array('eth0', 'en0', 'eth1', 'en1')) {
-    foreach ($ifaces as $iface) {
-        $output = shell_exec("/sbin/ifconfig $iface 2>&1");
-        if (preg_match("/([0-9A-F]{2}[:-]){5}([0-9A-F]{2})/", strtoupper($output), $m)) {
-            if (isset($m[0])) return trim($m[0]);
-        }
+    if (stripos(PHP_OS, 'WIN') === 0) {
+		$output = shell_exec("ipconfig /all");
+		if (preg_match("/Physical[^:]+:(.*)/i", $output, $m)) {
+			if (isset($m[1])) return str_replace('-', ':', trim($m[1]));
+		}
+	} else {
+	    foreach ($ifaces as $iface) {
+	        $output = shell_exec("/sbin/ifconfig $iface 2>&1");
+	        if (preg_match("/([0-9A-F]{2}[:-]){5}([0-9A-F]{2})/", strtoupper($output), $m)) {
+	            if (isset($m[0])) return trim($m[0]);
+	        }
+	    }
     }
+	return null;
 }
