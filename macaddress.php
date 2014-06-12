5bc7b559351c91104bfbef4a2622f78afea596b6
prasad83 <prasad83@users.noreply.github.com>
2014-06-12 16:49:21 +0530

    Update macaddress.php

--- macaddress.php
+++ macaddress.php
@@ -7,7 +7,8 @@
  * @return macaddress of the first successfully detected interface.
  */
 function findmacaddress($ifaces=array('eth0', 'en0', 'eth1', 'en1')) {
-    foreach ($ifaces as $iface) {$output = shell_exec("ifconfig $iface 2>&1");
+    foreach ($ifaces as $iface) {
+        $output = shell_exec("/sbin/ifconfig $iface 2>&1");
         if (preg_match("/([0-9A-F]{2}[:-]){5}([0-9A-F]{2})/", strtoupper($output), $m)) {
             if (isset($m[0])) return trim($m[0]);
         }
